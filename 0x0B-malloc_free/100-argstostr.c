2ef80c9b63070fb9b4fbe7cf79b9781900d8ee99
DamilolaIsrael01 <damilola.israel01@gmail.com>
2023-03-14 18:50:45 -0700

    5. It isn't the mountains ahead to climb that wear you out; it's the pebble in your shoe

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -33,10 +33,10 @@ char *argstostr(int ac, char **av)
 		{
 			str[r] = av[i][n];
 			r++;
-			if (str[r] == '\0')
-			{
-				str[r++] = '\n';
-			}
-			return (str);
 		}
-
+	if (str[r] == '\0')
+	{
+		str[r++] = '\n';
+	}
+	return (str);
+}
