31a54fdfa99e0c7f9849b38a58e39105c3b0c418
DamilolaIsrael01 <damilola.israel01@gmail.com>
2023-03-16 17:06:03 -0700

    5. We must accept finite disappointment, but never lose infinite hope

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -61,18 +61,12 @@ int main(int argc, char *argv[])
 	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 
 	s1 = argv[1], s2 = argv[2];
-
 	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
 		errors();
-
 	len1 = _strlen(s1);
-
 	len2 = _strlen(s2);
-
 	len = len1 + len2 + 1;
-
 	result = malloc(sizeof(int) * len);
-
 	if (!result)
 		return (1);
 	for (i = 0; i <= len1 + len2; i++)
@@ -81,15 +75,11 @@ int main(int argc, char *argv[])
 	{
 		digit1 = s1[len1] - '0';
 		carry = 0;
-
 		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
 		{
 			digit2 = s2[len2] - '0';
-
 			carry += result[len1 + len2 + 1] + (digit1 * digit2);
-
 			result[len1 + len2 + 1] = carry % 10;
-
 			carry /= 10;
 		}
 		if (carry > 0)
